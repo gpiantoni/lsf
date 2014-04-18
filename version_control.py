9b6a8108c0da0a107bb3c0527dd4529ac7aa8c4a
Gio on Eris <github@gpiantoni.com>
2014-04-18 14:06:41 -0400

    use common functions to keep track of the versions being used

---
+++ version_control.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+from subprocess import check_output
+import phypno
+
+phypno_path = phypno.__path__[0] + '/../.git'
+phypno_ver = check_output('git --git-dir ' + phypno_path + ' rev-parse HEAD', 
+                          shell=True).decode('utf-8')
+
+print('Last run on ' + str(datetime.now()) + '\n')
+print('Phypno Version: ' + phypno_ver)
+print(check_output('pip freeze', shell=True).decode('utf-8').replace('\n', ', '))
